844733aa3cfd110192cf5b3fea979ccd69b787ac
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-08 17:30:45 +0200

    aggiustato try catch - esercizio concluso

--- db.php
+++ db.php
@@ -5,18 +5,25 @@ require_once __DIR__ . '/Models/Toy.php';
 
 $croquettes = new Food($dog, 'crocchette', 2, 'image/croquettes.jpg', 'Ultima', 'pollo');
 $croquettes->setWeight(5, 'kg');
-$croquettes->setPrice(3.99);
-
-$kennel = new Kennel($dog, 'cuccetta', 5, 'image/kennel.jpg', 'comfort', 'legno');
-$kennel->setWeight(15, 'kg');
-$kennel->setPrice(119.99);
-
-$laser = new Toy($cat, 'laser', 4, 'image/laser.jpg', 'scimuni gatti', false);
-$laser->setWeight(100, 'g');
-$laser->setPrice(14.99);
-
 try {
     $croquettes->setPrice('prova errore');
 } catch (Exception $e) {
     echo "Si è verificato un errore: " . $e->getMessage();
 }
+
+$kennel = new Kennel($dog, 'cuccetta', 5, 'image/kennel.jpg', 'comfort', 'legno');
+$kennel->setWeight(15, 'kg');
+try {
+    $kennel->setPrice(119.99);
+} catch (Exception $e) {
+    echo "Si è verificato un errore: " . $e->getMessage();
+}
+
+$laser = new Toy($cat, 'laser', 4, 'image/laser.jpg', 'scimuni gatti', false);
+$laser->setWeight(100, 'g');
+try {
+    $laser->setPrice(14.99);
+} catch (Exception $e) {
+    echo "Si è verificato un errore: " . $e->getMessage();
+}
+
